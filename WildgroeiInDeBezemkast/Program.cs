53e84c0f1345e435a04579a61484e2bcf1fbb86a
ReinierJ <ReinierJ@ISNL-G78G344>
2024-08-13 14:30:31 +0200

    Add project files.

---
+++ WildgroeiInDeBezemkast/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WildgroeiInDeBezemkast.Models;
+
+Console.WriteLine("Hello, World!");
+
+var context = new AdventureWorks2022Context();
+
+Console.WriteLine(context.People.First().FirstName);
+
+await context.People.ExecuteDeleteAsync();
