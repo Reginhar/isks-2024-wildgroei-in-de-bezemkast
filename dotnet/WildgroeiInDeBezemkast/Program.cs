a0f445c736b16da466fd66c0e00ae52f6d0940d4
Reinier Joosse <Reinier.Joosse@infosupport.com>
2024-08-15 14:27:16 +0200

    Gebruik een connection string

--- dotnet/WildgroeiInDeBezemkast/Program.cs
+++ dotnet/WildgroeiInDeBezemkast/Program.cs
@@ -3,7 +3,11 @@ using WildgroeiInDeBezemkast.Models;
 
 Console.WriteLine("Hello, World!");
 
-var context = new AdventureWorks2022Context();
+const string connectionString = "Server=.;Database=AdventureWorks2022;Trusted_Connection=True;";
+
+var optionsBuilder = new DbContextOptionsBuilder<AdventureWorks2022Context>();
+optionsBuilder.UseSqlServer(connectionString);
+var context = new AdventureWorks2022Context(optionsBuilder.Options);
 
 Console.WriteLine(context.People.First().FirstName);
 
